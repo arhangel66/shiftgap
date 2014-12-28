1143e36bc7ebdf4469d620e22209b42f8e228dd5
MICHAEL MACKINNON <mike@eth0.ca>
2014-12-28 16:04:30 -0700

    basic test for shift listing

--- apps/shifts/tests.py
+++ apps/shifts/tests.py
@@ -1,10 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.http import HttpRequest
 from django.core.urlresolvers import resolve
 from .views import ShiftListing
 
 
-class SmokeTest(TestCase):
+class ShiftsTest(TestCase):
 
-    def test_url_resolves_to_shift_listing(self):
-        found = resolve('/shifts')
-        self.assertEqual(found.func, ShiftListing.as_view())
+    def test_returns_correct_html(self):
+        request_factory = RequestFactory()
+        request = request_factory.get('/shifts')
+        view = ShiftListing.as_view()
+        response = view(request)
+        response.render()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], 'shifts/shift_list.html')
+        self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
+        self.assertTrue(response.content.endswith(b'</html>'))
